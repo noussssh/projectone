16ea3231356a4758838c798963a43f2b3a027e13
noussssh <noussssh@gmail.com>
2024-04-16 21:07:40 +0400

    first commit

---
+++ sim/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Article
+
+# Create your views here.
+def articles(request):
+    """
+    Fetch paginated articles and render them
+    """
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(Article.objects.all(), 10)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'articles.html', {'page_obj': page_obj})
